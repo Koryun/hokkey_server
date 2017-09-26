eee40aa697f78d33f71f6d09a875957fac4c2c42
Koryun <k.movsesyan@gmail.com>
2017-09-25 22:03:44 -0700

    fix and modified

--- index.php
+++ index.php
@@ -4,90 +4,108 @@ define('DS', DIRECTORY_SEPARATOR);
 define('BASE_DIR', __DIR__);
 define('GAMES_DIR', BASE_DIR.DS.'games');
 
-if(isset($_GET['start']) && isset($_GET['player'])) {
-    $gamestat = array(
-        'angel' => rand(1, -1) * pi()/18,
-        'players' => array(
-            $_GET['player'] => array(
-                'score' => 0,
-                'x'     => 0,
-            ),
-        ),
-    );
-    $filename = 'start__'.md5($_GET['player']).'__'.time();
-    file_put_contents(GAMES_DIR.DS.$filename, json_encode($gamestat));
-    echo json_encode(array('success' => true, 'game' => $filename));
+function check() {
+    foreach(func_get_args() as $arg) {
+        if(!isset($_GET[$arg]))
+            return false;
+    }
+    return true;
 }
 
-if(isset($_GET['find']) && isset($_GET['player'])) {
-    echo json_encode(array('success' => true, 'games' => glob('start_*')));
+function get($name) {
+    return $_GET[$name];
 }
 
-if(isset($_GET['join']) && isset($_GET['game']) && isset($_GET['player'])) {
-    $gamestat = file_get_contents(GAMES_DIR.DS.$_GET['game']);
-    $gamestat = json_decode($gamestat, true);
-    $gamestat['players'][$_GET['player']]['score'] = 0;
-    $gamestat['players'][$_GET['player']]['x'] = 0;
-    unlink(GAMES_DIR.DS.$_GET['game']);
-    $id = key($gamestat['players']);
-    $name = 'game__'.md5($id).'__'.md5($_GET['player']).'__'.time();
-    file_put_contents(GAMES_DIR.DS.$name, json_encode($gamestat));
-    echo json_encode(array('success' => true, 'game' => $name));
+function response($result) {
+    $result['success'] = true;
+    echo json_encode($result)."\n\r";
 }
 
-if(isset($_GET['playing']) && isset($_GET['game'])) {
-    if(is_readable(GAMES_DIR.DS.$_GET['name'])) {
-        $gamestat = file_get_contents(GAMES_DIR.DS.$_GET['game']);
-        $gamestat = json_decode($gamestat, true);
-        if(isset($_GET['hit']))
-            $gamestat['angel'] = rand(1, -1)*pi()/18;
-        if(isset($_GET['win']) && isset($_GET['player']))
-            $gamestat['players'][$_GET['player']]['score']++;
-        if(isset($_GET['player']) && isset($_GET['x']))
-            $gamestat['players'][$_GET['player']]['x'] = $_GET['x'];
-        foreach($gamestat['players'] as $id => $player) {
-            if($id != $_GET['player']) {
-                $x = $players['x'];
-                break;
-            }
-        }
+function report($error) {
+    echo json_encode(['success' => false, 'error' => $error]);
+}
 
-        echo json_encode(array('success' => true, 'angel' => $gamestat['angel'], 'x' => $x));
-        unlink(GAMES_DIR.DS.$_GET['game']);
-        file_put_contents(GAMES_DIR.DS.$_GET['game'], json_encode($gamestat));
-    } else {
-        echo json_encode(array('success' => false));
+$_GET['player'] = 1;
+$_GET['playing'] = true;
+$_GET['game'] = 'game__1__2__201709260451';
+$_GET['position'] = 1;
+
+if(check('player') && in_array(get('player'), [1, 2])) {
+    if(check('start')) {
+        $id = get('player');
+        $gamestat = [
+            'angel'     => rand(1, -1) * pi()/18,
+            'players'   => [
+                $id     => [
+                    'score' => 0,
+                    'position'     => 0,
+                ],
+            ],
+        ];
+        $filename = 'start__'.$id.'__NULL__'.date('YmdHi', time());
+        $success = file_put_contents(GAMES_DIR.DS.$filename, json_encode($gamestat));
+        if($success)
+            response([]);
+        else
+            report('could not write file');
+    } elseif(check('find')) {
+        $time = date('YmdHi', time());
+        $mintime = $time - 5;
+        $result = glob(GAMES_DIR.DS."start__*__NULL__*");
+        array_walk($result, function(&$item, $key) {
+            $item = explode('__', $item);
+            if($item[1] != get('player'))
+                $item = $item[1];
+            else
+                $item = 0;         
+        });
+        response(['result' => $result]);
+    } elseif(check('join', 'game')) {
+        $time = date('YmdHi', time());
+        $mintime = $time - 5;
+        $pid = get('game');
+        $games = glob(GAMES_DIR.DS.'start__'.$pid."__NULL__*");
+        if(!empty($games)) {
+            $game = array_shift($games);
+            $gamestat = file_get_contents($game);
+            $gamestat = json_decode($gamestat, true);
+            $gamestat['players'][get('player')]['score'] = 0;
+            $gamestat['players'][get('player')]['position'] = 0;
+            $players = array_keys($gamestat['players']);
+            unlink($game);
+            $name = 'game__'.$players[0].'__'.$players[1].'__'.date('YmdHi', time());
+            $success = file_put_contents(GAMES_DIR.DS.$name, json_encode($gamestat));
+            if($success)
+                response([]);
+            else
+                report('could not write file');
+        } else
+            report('no file found');
+    } elseif(check('check')) {
+        $games = glob(GAMES_DIR.DS.'game__'.get('player').'__*__*');
+        if(empty($games))
+            $games = glob(GAMES_DIR.DS.'game__*__'.get('player').'__*');
+        if(!empty($games)) {
+            $game = array_shift($games);
+            response(['game' => str_replace(GAMES_DIR.DS, '', $game)]);
+        } else
+            report('no file');
+    } elseif(check('playing', 'game', 'position')) {
+        if(is_readable(GAMES_DIR.DS.get('game'))) {
+            $gamestat = file_get_contents(GAMES_DIR.DS.get('game'));
+            $gamestat = json_decode($gamestat, true);
+            $gamestat['players'][get('player')]['position'] = get('position');
+            if(check('hit'))
+                $gamestat['angel'] = rand(1, -1) * pi()/18;
+            if(check('win'))
+                $gamestat['players'][get('player')]['score']++;
+            $gamestat = json_encode($gamestat);
+            response(['gamestat' => $gamestat]);
+            unlink(GAMES_DIR.DS.get('game'));
+            file_put_contents(GAMES_DIR.DS.get('game'), $gamestat);
+        } else 
+            report('no file');
     }
 }
-
-/*
-define('DS', DIRECTORY_SEPARATOR);
-define('BASE_DIR', __DIR__);
-define('CLASS_DIR', BASE_DIR.DS.'classes');
-define('LOG_DIR', BASE_DIR.DS.'logs');
-
-function __autoload($class) {
-    if(is_readable(CLASS_DIR.DS.$class.'.php'))
-        require_once(CLASS_DIR.DS.$class.'.php');
-}
-
-function array_get(array $array, $key, $default = null) {
-    if(isset($array[$key]))
-        return $array[$key];
-    return $default;
-}
-
-if(($_GET['user'] == 'mykola' && $_GET['password'] == 'pass1') || ($_GET['user'] == 'koryun' && $_GET['password'] == 'pass2')) {
-    $obj = file_get_contents(BSE_DIR.DS.'current_state');
-    $obj = json_decode($obj, true);
-    if($_GET['user'] == 'mykola') {
-        $obj['player1']['y'] = array_get($_POST, 'x', $obj['player1']['y']);
-        $obj['player1']
-    }
-}
-
-//require_once(BASE_DIR.DS.'engine.php');
-
-//Engine::run();
 
 ?>
